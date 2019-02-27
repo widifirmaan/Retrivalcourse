e834a93d3f0f7cb1bf384a12104751c5fe96ce1f
Widi Firmansyah <admin@widifirmaan.cf>
2019-02-27 10:46:41 +0700

    Initial

---
+++ DocumentRetrieval/src/model/Posting.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Posting implements Comparable<Posting>{
+    private String term;
+    private Document document;
+    private int numberOfTerm = 1;
+    
+    
+    public Posting(Document document) {
+        this.document = document;
+    }
+
+    public Posting(String term, Document document) {
+        this.term = term;
+        this.document = document;
+    }
+    
+
+    /**
+     * @return the document
+     */
+    public Document getDocument() {
+        return document;
+    }
+
+    /**
+     * @param document the document to set
+     */
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    /**
+     * @return the term
+     */
+    public String getTerm() {
+        return term;
+    }
+
+    /**
+     * @param term the term to set
+     */
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    @Override
+    public int compareTo(Posting posting) {
+        return term.compareToIgnoreCase(posting.getTerm());
+    }
+
+    /**
+     * @return the numberOfTerm
+     */
+    public int getNumberOfTerm() {
+        return numberOfTerm;
+    }
+
+    /**
+     * @param numberOfTerm the numberOfTerm to set
+     */
+    public void setNumberOfTerm(int numberOfTerm) {
+        this.numberOfTerm = numberOfTerm;
+    }
+    
+}
